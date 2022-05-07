cd6d22f14c848830f726700d3b7d496c2d1abc4c
Nokiusz <mati.klakus@gmail.com>
2022-05-07 23:34:43 +0200

    fixed listItem bug

--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,87 @@
 import React, { useEffect, useContext } from 'react'
 import { GlobalContext } from '../context/Context';
-import ListItem from './ListItem.js';
+import { List as ListAnt, Avatar, Tag, Button } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const List = () => {
     const { transactionsData, showValues } = useContext(GlobalContext);
+    const defaultImgSrc = `${process.env.PUBLIC_URL}/img/categories/default.png`
+    const ImgSrc = `${process.env.PUBLIC_URL}/img/categories/`
+
+
+    const getPriorityColor = (p) => {
+        switch (p) {
+
+            case 'low':
+                return '#gold'
+
+            case 'medium':
+                return 'orange'
+
+            case 'high':
+                return 'orange'
+
+            case 'very high':
+                return 'red'
+
+            default:
+                return 'cyan'
+        }
+
+    }
+
+
+    const handleDelete = (item) => {
+        console.log(item.id)
+    }
+    const handleEdit = (item) => {
+        console.log(item.id)
+    }
 
     return (
         <>
 
-            {transactionsData.map(item => (
-                <ListItem
-                    key={item.id}
-                    id={item.id}
-                    description={item.description}
-                    value={showValues ? item.value : '???'}
-                    category={item.category}
-                    currency={item.currency}
-                    currencySymbol={item.currencySymbol}
-                    type={item.type}
-                    priority={item.priority}
-                    date={item.date} />
-            ))
-            }
+            <ListAnt
+                itemLayout="horizontal"
+                dataSource={transactionsData}
+
+                renderItem={item => (
+
+                    <ListAnt.Item
+                        actions={[
+                            <EditOutlined key="edit" onClick={() => handleEdit(item)} />,
+                            <DeleteOutlined key="delete" onClick={() => handleDelete(item)} />,
+                        ]}
+                    >
+
+                        <ListAnt.Item.Meta
+                            avatar={<img
+                                width={32}
+                                height={32}
+                                src={`${ImgSrc}${item.category}.png`}
+                                alt={item.category}
+                                onError={event => {
+                                    event.target.src = defaultImgSrc
+                                    event.onerror = null
+                                }}
+                            />}
+                            title={`${showValues ? item.value : `??? | ${item.date}`} 
+                            ${showValues ? `${item.currencyAcronym} | ${item.date}` :
+                                    `??? | ${item.date}`} `}
+                            description={item.description}
+
+                        />
+                        <div className="tags">
+                            <Tag color={getPriorityColor(item.priority)}>{item.priority}</Tag>
+                            <Tag color={item.type === 'expense' ? 'red' : 'green'}>{item.type}</Tag>
+                        </div>
+
+
+                    </ListAnt.Item>
+                )
+                }
+            />
+
         </>
     )
 }
