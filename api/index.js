0beae105e0e8501d9d553112839b448887bb60c3
Mateusz Kłakus <mati.klakus@gmail.com>
2022-06-20 09:19:00 +0200

    added check to work on my mac

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const os = require('os')
+const express = require("express");
+const cors = require("cors");
+const os = require("os");
 /* Routes */
-const transactionsRoute = require('./routes/Transactions');
-const categoriesRoute = require('./routes/Categories');
-const currenciesRoute = require('./routes/Currencies');
-const typesRoute = require('./routes/Types');
-const prioritiesRoute = require('./routes/Priorities');
-const attachmentsRoute = require('./routes/Attachments');
-const db = require('./db');
+const transactionsRoute = require("./routes/Transactions");
+const categoriesRoute = require("./routes/Categories");
+const currenciesRoute = require("./routes/Currencies");
+const typesRoute = require("./routes/Types");
+const prioritiesRoute = require("./routes/Priorities");
+const attachmentsRoute = require("./routes/Attachments");
+const db = require("./db");
 
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger-output.json')
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 
 const app = express();
 app.use(cors());
@@ -19,47 +19,48 @@ app.use(express.json());
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-    res.json({ "message": "Hello There" })
+app.get("/", (req, res) => {
+  res.json({ message: "Hello There" });
 });
 
-app.use('/api/transactions', transactionsRoute);
-app.use('/api/categories', categoriesRoute);
-app.use('/api/currencies', currenciesRoute);
-app.use('/api/types', typesRoute);
-app.use('/api/priorities', prioritiesRoute);
-app.use('/api/attachments', attachmentsRoute);
+app.use("/api/transactions", transactionsRoute);
+app.use("/api/categories", categoriesRoute);
+app.use("/api/currencies", currenciesRoute);
+app.use("/api/types", typesRoute);
+app.use("/api/priorities", prioritiesRoute);
+app.use("/api/attachments", attachmentsRoute);
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/api/lastTransaction", (req, res) => {
-    const query = `SELECT ROWID FROM 'transaction' ORDER BY ROWID DESC LIMIT 1`;
-    db.all(query, (err, rows) => {
-        if (err) {
-            console.error(err.message);
-            res.status(400).json({ "error": err.message })
-        }
-        console.log(rows);
-        res.json({ rows })
-    });
+  const query = `SELECT ROWID FROM 'transaction' ORDER BY ROWID DESC LIMIT 1`;
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      res.status(400).json({ error: err.message });
+    }
+    console.log(rows);
+    res.json({ rows });
+  });
 });
-const networkInterfaces = os.networkInterfaces();
-const ip = networkInterfaces.Ethernet[1].address;
-
+const networkInterfaces = os.networkInterfaces(); 
+console.log(networkInterfaces);
+const ip = networkInterfaces.Ethernet
+  ? networkInterfaces.Ethernet[1].address
+  : networkInterfaces.en0[1].address;
 
 //save ip variable to a json file in key "ip"
-const fs = require('fs');
+const fs = require("fs");
 const data = { ip };
-fs.writeFileSync('../public/ip.json', JSON.stringify(data));
-
+fs.writeFileSync("../public/ip.json", JSON.stringify(data));
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}/`);
-    console.log('local ip:', ip);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/`);
+  console.log("local ip:", ip);
 });
 
 /* ERROR IF ENDPOINT DOES NOT EXIST */
 app.use(function (req, res, next) {
-    res.status(404).send({ error: 'Not found' })
-})
+  res.status(404).send({ error: "Not found" });
+});
